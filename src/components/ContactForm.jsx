e43654b4d18dbd30a9dbf24272fafc55e2e63697
hilman <hpanser@gmail.com>
2024-11-07 20:09:07 +0700

    update contact form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import emailjs from '@emailjs/browser'
-import toast from "react-hot-toast";
+import toast, { Toaster } from "react-hot-toast";
 
 const ContactForm = () => {
     const [formData, setFormData] = useState({
@@ -64,7 +64,49 @@ const ContactForm = () => {
         }
     }
     return (
-        <div>ContactForm</div>
+        <div className="mx-auto max-w-3xl p-4" id="contact">
+            <Toaster />
+            <h2 className="my-8 text-center text-4xl font-semibold tracking-tighter">
+                Let's Connect
+            </h2>
+            <form onSubmit={handleSubmit}>
+                <div className="mb-4">
+                    <input type="text" id="name" name="name" value={formData.name}
+                        placeholder="Name" onChange={handleChange}
+                        className="mb-8 w-full appearance-none rounded-lg 
+                        border border-gray-900 bg-transparent px-3 py-2 text-sm
+                        focus:border-gray-400 focus:ouline-none" />
+                    {errors.name && (
+                        <p className="text-sm text-pink-700">{errors.name}</p>
+                    )}
+                </div>
+                <div className="mb-4">
+                    <input type="email" id="email" name="email" value={formData.email}
+                        placeholder="Email" onChange={handleChange}
+                        className="mb-8 w-full appearance-none rounded-lg 
+                        border border-gray-900 bg-transparent px-3 py-2 text-sm
+                        focus:border-gray-400 focus:ouline-none" />
+                    {errors.email && (
+                        <p className="text-sm text-pink-700">{errors.email}</p>
+                    )}
+                </div>
+                <div className="mb-4">
+                    <textarea id="message" name="message" value={formData.message}
+                        placeholder="Message" onChange={handleChange}
+                        className="mb-8 w-full appearance-none rounded-lg 
+                        border border-gray-900 bg-transparent px-3 py-2 text-sm
+                        focus:border-gray-400 focus:ouline-none" rows="4" />
+                    {errors.message && (
+                        <p className="text-sm text-pink-700">{errors.message}</p>
+                    )}
+                </div>
+                <button type="submit" className={`mb-8 w-full rounded bg-yellow-400 
+                    px-4 py-2 text-sm font-semibold text-slate-950 hover:bg-yellow-500
+                    ${isSending ? 'cursor-not-allowed opacity-50' : ''}`} disabled={isSending}>
+                    
+                    {isSending ? 'Sending...' : 'Send Message'}</button>
+            </form>
+        </div>
     )
 }
 
